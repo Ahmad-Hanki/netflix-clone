e7a37c8d190711182142403afba2e247b7e02ecf
ahmad <itxti909@gmail.com>
2024-04-20 20:10:32 +0300

    dynamic page

--- src/components/MovieVideo.tsx
+++ src/components/MovieVideo.tsx
@@ -1,5 +1,3 @@
-
-
 import { Button } from "@/components/ui/button";
 import prisma from "@/db/client";
 import MovieButtons from "./MovieButtons";
@@ -40,8 +38,17 @@ export default async function MovieVideo() {
           {data?.title}
         </h1>
         <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
-        <div>
-          <MovieButtons />
+        <div className="flex gap-x-3 mt-4">
+          <MovieButtons
+            age={data?.age as number}
+            duration={data?.duration as number}
+            id={data?.id as number}
+            overview={data?.overview as string}
+            releaseDate={data?.release as number}
+            title={data?.title as string}
+            youtubeUrl={data?.youtubeString as string}
+            key={data?.id}
+          />{" "}
         </div>
       </div>
     </div>
